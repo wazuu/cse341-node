a071ec4e19ffe1328b2e75d5c61a366d3fabb13c
wazuu <smi21105@byui.edu>
2023-01-07 22:58:39 -0800

    major changes

--- controller/contacts.js
+++ controller/contacts.js
@@ -2,7 +2,7 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
+  const result = await mongodb.getDb().db("auth").collection('contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
@@ -13,7 +13,7 @@ const getSingle = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDb()
-    .db()
+    .db("auth")
     .collection('contacts')
     .find({ _id: userId });
   result.toArray().then((lists) => {
