3449b7fc6e105ee1a9b1d8975286251fce904471
wazuu <smi21105@byui.edu>
2023-01-07 19:29:33 -0800

    lesson 2

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
-const routes = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
-routes.get('/', (req, res) => {
-  res.send('Wendy Smith');
-});
+router.use('/contacts', require('./contacts'))
 
-module.exports = routes;
+module.exports = router;
