83873e4fc561b84d8dbe72f4942efba3eb85faf6
wazuu <smi21105@byui.edu>
2023-01-11 20:00:09 -0800

    L4

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/contacts', require('./contacts'))
+router.use('/contacts', require('./contacts'));
+router.use('/', require('./swagger'));
 
 module.exports = router;
---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'L4 API',
+    description: 'L4'
+  },
+  host: 'cse341-lesson-4.onrender.com',
+  schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
