1d94797f658fde5fa9a013b3074cfd99d4233d5f
wazuu <smi21105@byui.edu>
2023-01-17 18:22:59 -0800

    fixes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 router.use('/', require('./swagger'));
-router.use('/contacts', require('./players'));
+router.use('/players', require('./players'));
 
 
 module.exports = router;
