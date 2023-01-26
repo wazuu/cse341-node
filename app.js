bb880154bf2083a8571801fcad961d2c59efe27e
wazuu <smi21105@byui.edu>
2023-01-25 18:44:12 -0800

    L7

--- app.js
+++ app.js
@@ -5,6 +5,30 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
 const app = express();
 
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
+
+
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
+
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
@@ -21,3 +45,4 @@ mongodb.initDb((err, mongodb) => {
     console.log(`Connected to DB and listening on ${port}`);
   }
 });
+
