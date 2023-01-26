ab447e4a22414f488563824a46ee52cb652b1af4
wazuu <smi21105@byui.edu>
2023-01-25 20:34:16 -0800

    more error handling

--- controller/players.js
+++ controller/players.js
@@ -56,6 +56,20 @@ const createContact = async (req, res) => {
 };
 
 const updateContact = async (req, res) => {
+  try {
+    if (
+      !req.body?.firstName ||
+      !req.body?.lastName ||
+      !req.body?.position ||
+      !req.body?.team ||
+      !req.body?.birthday ||
+      !req.body?.college ||
+      !req.body?.championships
+    ) {
+      console.log("Invalid");
+      res.status(400).send("Bad request");
+      return;
+    }
   const userId = new ObjectId(req.params.id);
   const contact = {
     firstName: req.body.firstName,
@@ -75,9 +89,16 @@ const updateContact = async (req, res) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while updating the contact.');
   }
+} catch (error) {
+  res.status(500).json(error);
+}
 };
 
 const deleteContact = async (req, res) => {
+  if (!req.params.id) {
+    res.status(400).send("Bad request");
+    return;
+  }
   const userId = new ObjectId(req.params.id);
   const response = await mongodb.getDb().db("auth").collection('players').remove({ _id: userId }, true);
   console.log(response);
